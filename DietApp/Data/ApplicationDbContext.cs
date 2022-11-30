286389b165c2c7253b5625757ed0fa064e5036bc
BurakUmutAlkan <burumalkan96@gmail.com>
2022-11-30 20:24:19 +0300

    Add project files.

---
+++ DietApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("name=MyConnection")
+        {
+        }
+    }
+}
